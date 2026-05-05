c1e0a7806ee0615a69b9811eedbae6e7d273e3d9
mark muraya <markchege10@gmail.com>
2026-05-05 21:18:50 +0300

    fixed a minor error

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Github, Twitter, Linkedin } from 'lucide-react';
+import { GitHub, Twitter, Linkedin } from 'lucide-react';
 
 const Footer = () => {
   return (
